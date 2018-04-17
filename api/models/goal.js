35ad57a5a997e56b90ddb84d08e2a8e70e301947
jhlin2337 <James@Jamess-MacBook-Air.local>
2018-04-16 23:21:16 -0700

    Changes to user model and jwt expiration

--- api/models/goal.js
+++ api/models/goal.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const goalSchema = mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, required: true},
     title: {type: String, required: true},
-    description: {type: String, required: true},
+    description: String,
     deadline: {type: Date, required: true},
     completed: {type: Boolean, default: false}
 });
